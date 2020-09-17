36ea65e08e7e951fbaf916875cafc1e76c624189
Jansan <jansan-26@hotmail.com>
2020-09-17 08:04:32 +0200

    class Bil added

--- Diagnos2Lastbilar/Program.cs
+++ Diagnos2Lastbilar/Program.cs
@@ -2,12 +2,42 @@
 
 namespace Diagnos2Lastbilar
 {
-    
+    class Bil
+    {
+        private string RegistreringsNumber;
+        private string Tillverkare;
+        private int Årtal;
+        private bool Besiktad;
+
+        public Bil(string registreringsNumber, string tillverkare, int årtal, bool besiktad)
+        {
+            RegistreringsNumber = registreringsNumber;
+            Tillverkare = tillverkare;
+            Årtal = årtal;
+            Besiktad = besiktad;
+        }
+        public override string ToString()
+        {
+            if (Besiktad)
+            {
+                return "\n\t\t" + Tillverkare + " (" + Årtal + ")"
+                    + "\n\t\t" + RegistreringsNumber
+                    + "\n\t\tBesiktad";
+            }
+            else
+            {
+                return "\n\t\t" + Tillverkare + " (" + Årtal + ")"
+                    + "\n\t\t" + RegistreringsNumber
+                    + "\n\t\tObesiktad";
+            }
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
