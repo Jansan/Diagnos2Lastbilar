215dfb325c5c006a7f85e9cf506cb783b4985d37
Jansan <jansan-26@hotmail.com>
2020-09-17 07:54:06 +0200

    Add project files.

---
+++ Diagnos2Lastbilar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Diagnos2Lastbilar
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
