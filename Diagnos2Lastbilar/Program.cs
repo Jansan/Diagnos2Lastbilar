209b037105719e731f9817bd67f73fd87855da9e
Jansan <jansan-26@hotmail.com>
2020-09-27 09:50:42 +0200

    testvikt lastbil added

--- Diagnos2Lastbilar/Program.cs
+++ Diagnos2Lastbilar/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Diagnos2Lastbilar
 {
     class Bil
     {
-        private string RegistreringsNumber;
-        private string Tillverkare;
-        private int Årtal;
-        private bool Besiktad;
+        protected string RegistreringsNumber;
+        protected string Tillverkare;
+        protected int Årtal;
+        protected bool Besiktad;
 
         public Bil(string registreringsNumber, string tillverkare, int årtal, bool besiktad)
         {
@@ -15,6 +16,10 @@ namespace Diagnos2Lastbilar
             Tillverkare = tillverkare;
             Årtal = årtal;
             Besiktad = besiktad;
+        }
+        public virtual string Körkort()
+        {
+            return "\t\tDe flesta former av bilar kräver en form av körkort, som minst med B-behörighet.";
         }
         public override string ToString()
         {
@@ -39,17 +44,81 @@ namespace Diagnos2Lastbilar
 
         public Lastbil(string registreringsNumber, string tillverkare, int årtal, bool besiktad, int vikt) : base(registreringsNumber, tillverkare, årtal, besiktad)
         {
+            RegistreringsNumber = registreringsNumber;
+            Tillverkare = tillverkare;
+            Årtal = årtal;
+            Besiktad = besiktad;
             Vikt = vikt;
         }
+        public override string Körkort()
+        {
+            return "\t\tLastbilar kräver variationer C-körkort för att framföras.";
+        }
+        public string ViktKontroll(int testVikt)
+        {
+            if (testVikt <= Vikt)
+            {
+                return "\t\tGodkänd test. Vikten " + testVikt + " av " + Vikt + " kan bäras av " + RegistreringsNumber + ".";
+            }
+            else
+            {
+                return "\t\tEj godkänd test. Vikten " + testVikt + " av " + Vikt + " kan ej bäras av " + RegistreringsNumber + ".";
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString() 
+                + "\n\t\tVikt " + Vikt;
+        }
 
-       
+
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Lastbil minLastbil = new Lastbil("ABC123", "Volvo", 1999, true, 64);
-            Console.WriteLine(minLastbil);
+            List<Bil> bilsLista = new List<Bil>();
+            bilsLista.Add(new Lastbil("HTR896", "Toyota", 2005, true, 85));
+            bilsLista.Add(new Lastbil("JKL564", "Volvo", 2015, false, 60));
+            bilsLista.Add(new Bil("DER256", "Porche", 2018, true));
+            bilsLista.Add(new Bil("ABC123", "Ford", 2010, true));
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("\n\n\t\t* Välkommen till Bilregistret. *\n\n\t\t[1] - Viktkontroll\n\t\t[2] - Samliga enheter\n\t\t[3] - Avsluta");
+                Console.Write("\n\t\t* ");
+                int result;
+                while (!Int32.TryParse(Console.ReadLine(), out result))
+                {
+                    Console.Write("\n\t\t* ");
+                }
+                switch (result)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("\n\n\t\t* Var god skriv in ett värde du vill testa mot de registrerade Lastbilarna *");
+                        Console.Write("\n\t\t* ");
+                        int testVikt;
+                        while (!Int32.TryParse(Console.ReadLine(), out testVikt))
+                        {
+                            Console.Write("\n\t\t* ");
+                        }
+                        foreach (Bil exempelBil in bilsLista)
+                        {
+                            if (exempelBil is Lastbil exempleLastbil)
+                            {
+                                Console.WriteLine("\t\t* " + exempleLastbil.ViktKontroll(testVikt));
+                            }
+                        }
+                        Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
     }
 }
